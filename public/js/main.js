b3a8b341272f3a7cdd1259e210ec174b9315a318
GaGiiiii <dragoslav.gagi8@yahoo.com>
2020-03-31 11:53:44 +0200

    Username already taken.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const origin = window.location.origin;   // Returns base URL (https://example.co
 const socket = io(origin);
 let span = document.getElementById('numberOfConnected');
 let joinButton = document.getElementById('join-btn');
+let usersFront = new Array();
 
 function checkUsers(users){
   span.innerText = users.length;
@@ -14,18 +15,21 @@ function checkUsers(users){
 
 socket.on('joinedUsersOnConnection', (users) => {
     checkUsers(users);
-    console.log(users)
+    usersFront = users;
 });
 
 socket.on('joinedUsersOnConnect', (users) => {
     checkUsers(users);
+    usersFront = users;
 });
 
 socket.on('joinedUsersOnDisconnect', (object) => {
-  checkUsers(object.users);
+    checkUsers(object.users);
 });
 
 function checkNicknameForm(){
+  const lengthError = document.getElementById('length-error');
+  const takenNicknameError = document.getElementById('taken-nickname-error')
   // Create a new div element
   let temporalDivElement = document.createElement("div");
   let nickname = document.getElementById('nickname');
@@ -35,12 +39,35 @@ function checkNicknameForm(){
   nickname.value = temporalDivElement.textContent || temporalDivElement.innerText || "";
 
   if(isValidNicknameLength(nickname.value)){
+
+    if(nicknameTaken(nickname.value)){
+      nickname.classList.add('is-invalid');
+
+      lengthError.style.display = "none";
+      takenNicknameError.style.display = "block";
+
+      return false;
+    }
+
     return true;
   }else{
     nickname.classList.add('is-invalid');
 
+    lengthError.style.display = "block";
+    takenNicknameError.style.display = "none";
+
     return false;
   }
+}
+
+function nicknameTaken(nickname){
+  for(let i = 0; i < usersFront.length; i++){
+    if(usersFront[i].username == nickname){
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function isValidNicknameLength(nickname){
