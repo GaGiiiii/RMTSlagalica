57784b6cf9466b285c811eb3f5ca7bc4b938e323
GaGiiiii <dragoslav.gagi8@gmail.com>
2020-08-26 01:11:03 +0200

    PW Update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 /* ********** DB CONFIG ********** */
 
 module.exports = {
-    database: 'mongodb://GaGiiiii:gagi123...0@ds161620.mlab.com:61620/rmtslagalica',
+    database: 'mongodb://GaGiiiii:gagi123@ds161620.mlab.com:61620/rmtslagalica',
     secret: 'rmt'
   }
